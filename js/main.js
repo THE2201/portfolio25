fe60ae050ca01699696cede4659dd89dc8d70569
AbelErnesto <correooficial58@gmail.com>
2025-04-22 13:27:26 -0600

    styles docs, themes, funcionality

--- js/main.js
+++ js/main.js
@@ -1,15 +1,38 @@
+const body = document.body;
+const themeLabel = document.getElementById("themeLabel");
+
+
+let currentLang = localStorage.getItem("lang") || "en";
+
+function setLanguage(lang) {
+  currentLang = lang;
+  localStorage.setItem("lang", lang);
+
+  const elements = document.querySelectorAll("[data-en]");
+  elements.forEach(el => {
+    el.innerHTML = el.getAttribute(`data-${lang}`);
+  });
+}
+
+//alternar entre spa eng
+function toggleLanguage() {
+  const newLang = currentLang === "en" ? "es" : "en";
+  setLanguage(newLang);
+}
+//cargar idioma
+setLanguage(currentLang);
+
+
 const isDarkMode =
   window.matchMedia &&
   window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 if (isDarkMode) {
   console.log("System uses Dark Mode");
-  document.body.style.backgroundColor = "black";
-  document.body.style.color = "white";
+ 
 } else {
   console.log("System uses light mode");
-  document.body.style.backgroundColor = "white";
-  document.body.style.color = "black";
+  
 }
 
 const systemPrefersDark = window.matchMedia(
@@ -24,9 +47,22 @@ document.documentElement.setAttribute("data-theme", initialTheme);
 // Toggle function
 function toggleTheme() {
   const current = document.documentElement.getAttribute("data-theme");
+  console.log(current);
   const newTheme = current === "dark" ? "light" : "dark";
+  console.log(newTheme);
   document.documentElement.setAttribute("data-theme", newTheme);
   localStorage.setItem("theme", newTheme);
+
+  // Toggle theme class
+  body.classList.toggle("dark-theme");
+
+  // Update label based on current theme
+  if (body.classList.contains("dark-theme")) {
+    themeLabel.innerHTML = '<i class="fa-solid fa-sun"></i> <i class="fa-solid fa-toggle-on"></i> ';
+  } else {
+    themeLabel.innerHTML = '<i class="fa-solid fa-moon"></i> <i class="fa-solid fa-toggle-off"></i> ';
+  }
+
   console.log("Theme applied");
 }
 
@@ -63,3 +99,22 @@ $(document).ready(function () {
     }
   });
 });
+
+
+
+const themeChange = window.matchMedia('(prefers-color-scheme: dark)');
+    
+      // Detect initial theme
+      const setTheme = (isDark) => {
+        console.log(isDark ? 'Dark mode' : 'Light mode');
+        // You can also add/remove a class or change styles here
+        document.documentElement.setAttribute('data-theme', isDark ? 'dark' : 'light');
+      };
+    
+      // Initial theme
+      setTheme(themeChange.matches);
+    
+      // Watch for changes
+      themeChange.addEventListener('change', (e) => {
+        setTheme(e.matches);
+      });
